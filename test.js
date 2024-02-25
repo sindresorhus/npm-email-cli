1523654caf0083f72d13c82c09a890d26adfda08
tommy-mitchell <tmitchell7@uh.edu>
2024-02-25 11:55:51 -0600

    Target Node.js 18, update dependencies

--- test.js
+++ test.js
@@ -5,3 +5,13 @@ test('main', async t => {
 	const {stdout} = await execa('./cli.js', ['sindresorhus']);
 	t.is(stdout, 'sindresorhus@gmail.com');
 });
+
+test('no username', async t => {
+	const {stderr} = await t.throwsAsync(execa('./cli.js'));
+	t.is(stderr, 'Specify a npm username');
+});
+
+test('no email found', async t => {
+	const {stderr} = await t.throwsAsync(execa('./cli.js', ['sindresorhus123']));
+	t.is(stderr, 'Could not find either user or email');
+});
